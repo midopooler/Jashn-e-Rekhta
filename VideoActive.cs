f876957c6375484d0dbfc4f32445c4898e7ab74d
Pulkit Midha <midhapulkit28@gmail.com>
2019-12-21 00:55:44 +0530

    Add files via upload

---
+++ VideoActive.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoActive : MonoBehaviour
+{
+    MeshRenderer MR;
+    VideoPlayer AS;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MR = transform.GetComponent<MeshRenderer>();
+        AS = transform.GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(MR.enabled == true)
+        {
+            AS.enabled = true;
+        }
+        else
+        {
+            AS.enabled = false;
+        }
+    }
+}
